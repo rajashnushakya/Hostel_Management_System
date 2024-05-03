7d8ba1e6235524bcec044f5419920b3ca4cefc83
Rajashnu <rajashnushakya@gmail.com>
2024-05-03 12:56:43 +0545

    missing bracket added

--- php/read.php
+++ php/read.php
@@ -1,21 +1,21 @@
 <?php
-
+require 'function.php';
 header('Access-Control-Allow-Origin: *'); 
 header('Content-Type: application/json');
-header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET');
 header('Access-Control-Allow-Headers: Content-Type, Acces-Control-Allow-Headers, Authorization, X-Requested-With');
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 if($requestMethod == 'GET') {
-    $residentList = getCustomerList();
+    $residentList = getResidentList();
     echo $residentList;
 }else{
-    $data = {
-        'status'=> 405,
-        'message'=> $requestMethod. 'Method Not Allowed', 
-    };
-    header ("HTTP/1.0  405 Method Not Allowed");
-    echo json_encode( $data );
+    $data = $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed', 
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
 }
 
 ?>
