ad187f3895cd22530d8ab819063f4457b267c2b5
Aditihamal <hamaladiti64@gmail.com>
2024-05-05 17:23:38 +0545

    Registration backend commit

---
+++ registration.php
@@ -0,0 +1,240 @@
+<?php
+
+include('connection.php');
+
+if(isset($_POST['submit'])) {
+    $fname=$_POST['fname'];
+    $mname=$_POST['mname'];
+    $lname=$_POST['lname'];
+    $gender=$_POST['gender'];
+    $contactno=$_POST['contact'];
+    $emailid=$_POST['email'];
+    $password=$_POST['password'];
+    $role = $_POST['Role'];
+    
+    $query="insert into  userRegistration(firstName,middleName,lastName,gender,contactNo,email,password,role) values(?,?,?,?,?,?,?,?)";
+    $stmt = $mysqli->prepare($query);
+    $rc=$stmt->bind_param('sssssiss',$fname,$mname,$lname,$gender,$contactno,$emailid,$password,$role);
+    $stmt->execute();
+    
+    if($stmt->error) {
+        echo "Error: " . $stmt->error;
+    } else {
+        echo "<script>alert('Student Successfully registered');</script>";
+    }
+}
+?>
+
+
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	<title>User Registration</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">>
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<link rel="stylesheet" href="css/style.css">
+<script type="text/javascript" src="js/jquery-1.11.3-jquery.min.js"></script>
+<script type="text/javascript" src="js/validation.min.js"></script>
+<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
+<script type="text/javascript">
+function valid()
+{
+if(document.registration.password.value!= document.registration.cpassword.value)
+{
+alert("Password and Re-Type Password Field do not match  !!");
+document.registration.cpassword.focus();
+return false;
+}
+return true;
+}
+</script>
+</head>
+<body>
+
+		<div class="content-wrapper">
+			<div class="container-fluid">
+
+				<div class="row">
+					<div class="col-md-12">
+					
+						<h2 class="page-title">Staff Registration </h2>
+
+						<div class="row">
+							<div class="col-md-12">
+								<div class="panel panel-primary">
+									<div class="panel-heading">HMS</div>
+									<div class="panel-body">
+			<form method="post" action="" name="registration" class="form-horizontal" onSubmit="return valid();">
+											
+										
+
+
+<div class="form-group">
+<label class="col-sm-2 control-label">First Name : </label>
+<div class="col-sm-8">
+<input type="text" name="fname" id="fname"  class="form-control" required="required" >
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Middle Name : </label>
+<div class="col-sm-8">
+<input type="text" name="mname" id="mname"  class="form-control">
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Last Name : </label>
+<div class="col-sm-8">
+<input type="text" name="lname" id="lname"  class="form-control" required="required">
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Gender : </label>
+<div class="col-sm-8">
+<select name="gender" class="form-control" required="required">
+<option value="">Select Gender</option>
+<option value="male">Male</option>
+<option value="female">Female</option>
+<option value="others">Others</option>
+</select>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Role : </label>
+<div class="col-sm-8">
+<select name="Role" class="form-control" required="required">
+<option value="">Select Role</option>
+<option value="Receptionist">Receptionist</option>
+<option value="Maintenance">Maintenance Staff</option>
+</select>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Contact No : </label>
+<div class="col-sm-8">
+<input type="text" name="contact" id="contact"  class="form-control" required="required">
+</div>
+</div>
+
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Email id: </label>
+<div class="col-sm-8">
+<input type="email" name="email" id="email"  class="form-control" onBlur="checkAvailability()" required="required">
+<span id="user-availability-status" style="font-size:12px;"></span>
+</div>
+</div>
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Password: </label>
+<div class="col-sm-8">
+<input type="password" name="password" id="password"  class="form-control" required="required">
+</div>
+</div>
+
+
+<div class="form-group">
+<label class="col-sm-2 control-label">Confirm Password : </label>
+<div class="col-sm-8">
+<input type="password" name="cpassword" id="cpassword"  class="form-control" required="required">
+</div>
+</div>
+						
+
+
+
+<div class="col-sm-6 col-sm-offset-4">
+<button class="btn btn-default" type="submit">Cancel</button>
+<input type="submit" name="submit" Value="Register" class="btn btn-primary">
+</div>
+</form>
+
+									</div>
+									</div>
+								</div>
+							</div>
+						</div>
+							</div>
+						</div>
+					</div>
+				</div> 	
+			</div>
+		</div>
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
+</body>
+	<script>
+function checkAvailability() {
+
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'emailid='+$("#email").val(),
+type: "POST",
+success:function(data){
+$("#user-availability-status").html(data);
+$("#loaderIcon").hide();
+},
+error:function ()
+{
+event.preventDefault();
+alert('error');
+}
+});
+}
+</script>
+<script type="text/javascript">
+function valid() {
+    // Check if password and confirm password match
+    if (document.registration.password.value !== document.registration.cpassword.value) {
+        alert("Password and Re-Type Password Field do not match!");
+        document.registration.cpassword.focus();
+        return false;
+    }
+
+    // Validate email format
+    var email = document.registration.email.value;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid email address.");
+        document.registration.email.focus();
+        return false;
+    }
+
+    // Validate contact number format (exactly 10 digits)
+    var contact = document.registration.contact.value;
+    var contactRegex = /^\d{10}$/;
+    if (!contactRegex.test(contact)) {
+        alert("Please enter a 10-digit contact number.");
+        document.registration.contact.focus();
+        return false;
+    }
+
+    return true;
+}
+</script>
+
+</html>
