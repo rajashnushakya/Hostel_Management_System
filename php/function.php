f52fc148cfe8de8e5deafbc6ea162c0fef319b9f
Rajashnu <rajashnushakya@gmail.com>
2024-05-05 11:04:54 +0545

    function with read

---
+++ php/function.php
@@ -0,0 +1,41 @@
+<?php
+require 'function.php';
+header('Access-Control-Allow-Origin: *'); 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type, Acces-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+if($requestMethod == 'GET') {
+    $residentList = getResidentList();
+    echo $residentList;
+}else{
+    $data = $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed', 
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?><?php
+require 'function.php';
+header('Access-Control-Allow-Origin: *'); 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type, Acces-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+if($requestMethod == 'GET') {
+    $residentList = getResidentList();
+    echo $residentList;
+}else{
+    $data = $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed', 
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
