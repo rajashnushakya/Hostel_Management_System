20b927d11b63cde48416e30417591f338071efc8
thapakabita852 <thapakabita852@gmail.com>
2024-05-13 17:40:51 +0545

    added js for pop up in index html page

---
+++ js/loginFormController.js
@@ -0,0 +1,7 @@
+document.getElementById("loginButton").addEventListener("click", function () {
+  document.getElementById("loginForm").style.display = "block";
+});
+
+document.getElementById("closeIcon").addEventListener("click", function () {
+  document.getElementById("loginForm").style.display = "none";
+});
