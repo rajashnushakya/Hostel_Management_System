cc65c22b74606e16923bb2a4d51e9412365f5e43
Rajashnu <rajashnushakya@gmail.com>
2024-05-14 21:07:39 +0545

    removing unnecessary tag

--- php/admindashboard.php
+++ php/admindashboard.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin Dashboard</title>
   <link rel="stylesheet" href="../css/admin.css" />
-  <link rel="stylesheet" href="responsive.css" />
 </head>
 <body>
   <!-- for header part -->
